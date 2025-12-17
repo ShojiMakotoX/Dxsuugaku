93e0187c99d627d4d996653e7b75d4c27a23a9bb
ShojiMakotoX <250039@jc-21.jp>
2025-12-17 13:21:16 +0900

    三角形表示

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,19 @@
 #include "Player.h"
 #include "DxLib.h"
 #include <cmath>
+#include "globals.h"
+
+
+namespace
+{
+	Vector2D World2Screen(const Vector2D& wpos)
+	{
+		Vector2D tmp;//tmporary = 作業領域
+		tmp.x = wpos.x;
+		tmp.y = WIN_HEIGHT - wpos.y;
+		return{tmp};
+	}
+}
 
 //引数なしコンストラクタ
 //適当な値を入れるだけ、して初期化していないメンバ変数を隠す
@@ -32,21 +45,36 @@ Player::~Player()
 void Player::Update()
 {
 	const float PI = 3.1415926359;
-	//3角形の3頂点をdir_とradius_とpos_から求めていくよ
-	vertex_[0] = { pos_.x + 0,pos_.y + radius_ };
-	vertex_[1] = { pos_.x + radius_ * cos(-60.0f * (PI / 180.0f)),
-		pos_.y + radius_ * sin(-60.0f * (PI / 180.0f)) };
-	vertex_[2] = { pos_.x + radius_ * cos(240.0f * (PI / 180.0f)),
-		pos_.y + radius_ * sin(240.0f * (PI / 180.0f)) };
 
-}
+	//原点にある半径1の内接する三角形を考える
+	Vector2D p[3];
+	p[0] = { 0.0f,1.0f };
+	p[1] = { cos(-60.0f * (PI / 180.0f)), sin(-60.0f * (PI / 180.0f)) };
+	p[2] = { cos(240.0f * (PI / 180.0f)), sin(240.0f * (PI / 180.0f)) };
+
+	p[0] = radius_ * p[0].x;p[0].y = radius_ * p[0].y;
+	p[1] = radius_ * p[1].x;p[1].y = radius_ * p[1].y;
+	p[2] = radius_ * p[2].x;p[2].y = radius_ * p[2].y;
+
+
+	//3角形の3頂点をdir_とradius_とpos_から求めていくよ
+	vertex_[0] = { pos_.x + p[0].x,pos_.y + p[0].y };
+	vertex_[1] = { pos_.x + p[1].x,pos_.y + p[1].y };
+	vertex_[2] = { pos_.x + p[2].x,pos_.y + p[2].y };
+
+};
 
 void Player::Draw()
 {
+	Vector2D scrPos[3];
+	scrPos[0] = World2Screen(vertex_[0]);
+	scrPos[1] = World2Screen(vertex_[0]);
+	scrPos[2] = World2Screen(vertex_[0]);
+
 	DrawTriangleAA(
-		vertex_[0].x, vertex_[0].y,
-		vertex_[1].x, vertex_[1].y,
-		vertex_[2].x, vertex_[2].y,
+		scrPos[0].x, WIN_HEIGHT - scrPos[0].y,
+		scrPos[1].x, WIN_HEIGHT - scrPos[1].y,
+		scrPos[2].x, WIN_HEIGHT - scrPos[2].y,
 		GetColor(255, 0, 0), TRUE
 	);
 }
